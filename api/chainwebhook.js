83845211d26423f1add6acf33606f5c4d5fed5c6
MichalGustafik <56283597+MichalGustafik@users.noreply.github.com>
2025-07-16 20:06:52 +0200

    Update chainwebhook.js

--- api/chainwebhook.js
+++ api/chainwebhook.js
@@ -69,6 +69,9 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        pinataMetadata: {
+          name: "metadata.json" // ✅ Názov súboru
+        },
         pinataContent: metadata
       }),
     });
